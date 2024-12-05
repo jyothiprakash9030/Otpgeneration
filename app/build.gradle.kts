af6b5c5412f3c2b96de5f93c2f815a11149a52a4
jyothiprakash1213 <jyothiprakash@vilvanetworks.com>
2024-12-05 08:36:00 +0530

    second commit otpGeneration completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -30,6 +32,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
@@ -40,9 +48,20 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation (libs.firebase.auth)
     implementation (libs.firebase.auth.v2120)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
 
 }
--- app/src/main/java/com/example/otpgenerations/Verifyenterotptwo.java
+++ app/src/main/java/com/example/otpgenerations/Verifyenterotptwo.java
@@ -1,21 +1,36 @@
 package com.example.otpgenerations;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
 public class Verifyenterotptwo extends AppCompatActivity {
 EditText inputotp1,inputotp2,inputotp3,inputotp4,inputotp5,inputotp6;
+String getoptbackend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,13 +46,50 @@ EditText inputotp1,inputotp2,inputotp3,inputotp4,inputotp5,inputotp6;
         inputotp5=findViewById(R.id.inputotp5);
         inputotp6=findViewById(R.id.inputotp6);
         TextView textView=findViewById(R.id.textmobileshownumber);
+
         textView.setText(String.format("+91-%s",getIntent().getStringExtra("mobile")));
 
+        getoptbackend=getIntent().getStringExtra("backend");
+        ProgressBar progressBarverifyotp=findViewById(R.id.verifyotp);
         verifybuttonclick.setOnClickListener(view ->{
 
 
             if(!inputotp1.getText().toString().trim().isEmpty()&&!inputotp2.getText().toString().trim().isEmpty()&&!inputotp3.getText().toString().trim().isEmpty()&&!inputotp4.getText().toString().trim().isEmpty()&&!inputotp5.getText().toString().trim().isEmpty()&&!inputotp6.getText().toString().trim().isEmpty()&&!inputotp1.getText().toString().trim().isEmpty())
             {
+                String entercodeotp=inputotp1.getText().toString()+
+                        inputotp2.getText().toString()+
+                        inputotp3.getText().toString()+
+                        inputotp4.getText().toString()+
+                        inputotp5.getText().toString()+
+                        inputotp6.getText().toString();
+                if(getoptbackend!=null)
+                {
+                    progressBarverifyotp.setVisibility(view.VISIBLE);
+                    verifybuttonclick.setVisibility(view.INVISIBLE);
+                    PhoneAuthCredential phoneAuthCredential= PhoneAuthProvider.getCredential(getoptbackend,entercodeotp);
+                    FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressBarverifyotp.setVisibility(view.VISIBLE);
+                            verifybuttonclick.setVisibility(view.INVISIBLE);
+                            if(task.isSuccessful())
+                            {
+                                Intent intent=new Intent(getApplicationContext(),Dashboard.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            }
+                            else
+                            {
+                                Toast.makeText(Verifyenterotptwo.this, "Please correct otp", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+                else
+                {
+                    Toast.makeText(this, "Please check  internet connection", Toast.LENGTH_SHORT).show();
+                }
+
                 Toast.makeText(Verifyenterotptwo.this, "Otp verify", Toast.LENGTH_SHORT).show();
             }
             else
@@ -47,13 +99,74 @@ EditText inputotp1,inputotp2,inputotp3,inputotp4,inputotp5,inputotp6;
             });
         numbertomove();
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
 
 
+
+
+
+
+        TextView resedlabel=findViewById(R.id.textresendotp);
+        resedlabel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                 
+                 
+                                         PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                                                 "+91" + getIntent().getStringExtra("mobile"), 60,
+                                                 TimeUnit.SECONDS, Verifyenterotptwo.this,
+                                                 new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                                                     @Override
+                                                     public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+
+                                                     }
+
+                                                     @Override
+                                                     public void onVerificationFailed(@NonNull FirebaseException e) {
+
+
+                                                         Toast.makeText(Verifyenterotptwo.this,e.getMessage(), Toast.LENGTH_SHORT).show();
+                                                     }
+
+                                                     @Override
+                                                     public void onCodeSent(@NonNull String newbackendotp, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+
+                                                                     getoptbackend=newbackendotp;
+                                                         Toast.makeText(Verifyenterotptwo.this, "Resend otp sucessfully", Toast.LENGTH_SHORT).show();
+
+
+
+
+
+                                                     }
+                                                 }
+
+
+                                         );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         ///////////////////////
+            }
         });
+
     }
 
     private void numbertomove()
